777e629c2f0ca21906e27a1f4ea1bf4848f63224
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-11-04 19:40:42 -0400

    initial commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,168 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(77);
+    std::cout << "Window Width: " << ofGetWindowWidth() << "   Window Height: " << ofGetWindowHeight() << endl;
+    newAgent.build(boxWidth, boxHeight, boxDepth);
+    
+    box.set(boxWidth, boxHeight, boxDepth, resolution, resolution, resolution);
+    box.pan(30); // roating the box 20 degrees to the right
+    
+    
+}
+
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    // newAgent.update();
+    
+    
+    
+}
+
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    // newAgent.draw();
+    
+    box.setPosition(ofGetWindowWidth()/2, ofGetWindowHeight()/2, 100);
+    
+    box.drawWireframe();
+    box.drawAxes(10);
+}
+
+
+
+
+//--------------------------------------------------------------
+//          AGENT CLASS
+//--------------------------------------------------------------
+
+void Agent::build(int x, int y, int z) {
+    
+    containerSizeX = x;
+    containerSizeY = y;
+    containerSizeZ = z;
+    
+    position.set(0, 0, 0);
+    setRandomPosition(containerSizeX, containerSizeY, containerSizeZ);
+    
+    offset = 10; // use to be set at 10000
+    stepSize = ofRandom(1, 8);
+    
+    ribbon.build(position, int(ofRandom(50, 150)) ); // may have to change this random deleration from int to float. is expecting int
+    
+    
+    
+    std::cout << "Position: " << position << endl;
+    std::cout << "StepSize: " << stepSize << endl;
+}
+
+
+void Agent::update() {
+    
+    angleY = ofNoise(position.x / noiseScale, position.y / noiseScale, position.z / noiseScale) * noiseStrength;
+    //
+    // a second, random angle is needed for an agent to move in all three dimensions.
+    // To produce this agnle, the point at which the random number is selected is simply
+    // shifted by the value of 'offset'
+    //
+    angleZ = ofNoise( (position.x / noiseScale) + offset, position.y / noiseScale, position.z / noiseScale) * noiseStrength;
+    
+    
+    // the new point of the agent is defined by the two angles 'angleY' and 'angleZ'
+    position.x += cos(angleZ) * cos(angleY) * stepSize;
+    position.y += sin(angleZ) * stepSize;
+    position.z += cos(angleZ) * sin(angleY) * stepSize;
+    
+    
+    // checking to see if agent is outside of the given container dimensions (not window)
+    if (position.x<0 || position.x>containerSizeX ||
+        position.y<0 || position.y>containerSizeY ||
+        position.z<0 || position.z>containerSizeZ) {
+        
+        setRandomPosition(containerSizeX, containerSizeY, containerSizeZ);
+        isOutside = true;
+    }
+    
+    ribbon.update(position, isOutside);
+    isOutside = false;
+    
+}
+
+
+void Agent::draw() {
+    ribbon.drawLineRibbon(ofColor(0, 0, 0), 2.0);
+}
+
+
+void Agent::setRandomPosition(int x, int y, int z) {
+    position.set(ofRandom(0, x), ofRandom(0, y), ofRandom(0, z));
+    
+}
+
+
+
+//--------------------------------------------------------------
+//          RIBBON CLASS
+//--------------------------------------------------------------
+
+
+
+void Ribbon3d::build(ofVec3f _position, int _count) {
+    
+    count = _count;
+    positionArray.resize(count); // init array with predifined size (count is size)
+    isGapArray.resize(count); // init array with predifined size (count is size)
+    
+    // declare the location for every ofVec3f point in the ribbon
+    for (int i = 0; i < count; i++) {
+        positionArray[i] = ofVec3f(_position.x, _position.y, _position.z);
+        isGapArray[i] = false;
+    }
+    
+}
+
+
+
+// In each step, the values are shifted on step to the right (in the array) to make
+// room for the new value.  In doing this, the oldest value is overwritten / erased
+
+void Ribbon3d::update(ofVec3f _position, bool _isGap) {
+    for (int i = count-1; i > 0; i--) {
+        positionArray[i].set(positionArray[i-1]);// setting the ofVec3f object to previous one
+        isGapArray[i] = isGapArray[i-1];
+    }
+    
+    // setting new value top beggining of array
+    positionArray[0].set(_position);
+    isGapArray[0] = _isGap;
+}
+
+
+
+// DRAWING THE RIBBON
+
+void Ribbon3d::drawLineRibbon(ofColor _strokeColor, float _width) {
+    
+    for (int i = 0; i < count; i++) {
+        line.addVertex(positionArray[i]);  // add a point to the end of the line from positionsArray
+        if (isGapArray[i] == true) {
+            line.clear();  // if a point is outside the container, delete all points in line and start again
+        }
+    }
+    ofSetColor(_strokeColor);
+    ofFill();
+    line.draw();  // draw the line
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/ofApp.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include "ofMain.h"
+
+
+//--------------------------------------------------------------
+//          RIBBON CLASS
+//--------------------------------------------------------------
+
+class Ribbon3d {
+public:
+    void build(ofVec3f _position, int _count);
+    void update(ofVec3f _position, bool _isGap);
+    void drawLineRibbon(ofColor _strokeColor, float _width);
+    
+    
+    int count; // how many previous positions are saved to arrays
+    vector <ofVec3f> positionArray;
+    vector <bool> isGapArray;
+    
+    ofPolyline line;
+    
+    // each 'ribbon' has a certain size/length to it, which is determined by
+    // the 'count' variable.  Each time a new ribbon is intantiated, the 'count'
+    // variable is randomized.
+    // the 'count' variable effects the length/size of the two arrays within the
+    // ribbon class (ie. 'isGapArray', 'positionArray')
+    
+    
+};
+
+
+//--------------------------------------------------------------
+//          AGENT CLASS
+//--------------------------------------------------------------
+
+// the agent class represents the 'starting point' for each 'ribon class'
+// there is one ribbon for every agent (i think.)
+
+class Agent {
+public:
+    void update();
+    void build(int x, int y, int z);
+    void draw();
+    void setRandomPosition(int x, int y, int z);
+    
+    // PRESET VARIABLES which alter the sketch based on their values
+    float noiseStrength = 20;
+    float noiseScale = 150;
+    
+    // the same as spazeSize variable in ofApp variable decloration
+    int containerSizeX;
+    int containerSizeY;
+    int containerSizeZ;
+    
+    bool isOutside = false;
+    ofVec3f position;
+    
+    float offset;
+    float stepSize;
+    float angleY;
+    float angleZ; // random angle needed for agent to move in 3 dimensions
+    
+    
+    // declare the ribbon class
+    Ribbon3d ribbon;
+    
+};
+
+
+//--------------------------------------------------------------
+//          OPENFRAMEWORKS APPLICATION
+//--------------------------------------------------------------
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+    
+    // ofBoxPrimitive to contain the agents being drawn.
+    
+    ofBoxPrimitive box;
+    
+    float boxWidth = 300;
+    float boxHeight = 400;
+    float boxDepth = 300;
+    
+    int resolution = 1;  // how many points are used to create shape
+    
+    
+    Agent newAgent;
+		
+};
+
